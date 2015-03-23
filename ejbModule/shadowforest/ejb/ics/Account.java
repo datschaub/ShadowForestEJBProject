cd432496ef6095c45f68980ad8c48f676d3980a3
datschaub <linus__k@hotmail.com>
2015-03-23 12:28:23 +0100

    mappning

--- ejbModule/shadowforest/ejb/ics/Account.java
+++ ejbModule/shadowforest/ejb/ics/Account.java
@@ -1,11 +1,15 @@
 package shadowforest.ejb.ics;
 
 import java.io.Serializable;
+import java.util.Set;
 import java.util.UUID;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -15,7 +19,8 @@ public class Account implements Serializable{
 	private String accId;
 	private String accName;
 	private String password;
-	
+	private Set<Character> characters;
+
 	@Id
 	@Column(name="AccId")
 	public String getAccId() {
@@ -42,6 +47,15 @@ public class Account implements Serializable{
 	
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	@OneToMany(mappedBy="account", fetch=FetchType.EAGER)
+	public Set<Character> getCharacters() {
+		return characters;
+	}
+
+	public void setCharacters(Set<Character> characters) {
+		this.characters = characters;
 	}
 
 }
